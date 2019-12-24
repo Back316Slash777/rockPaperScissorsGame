ce9254811fc8d515b0e7d15e57bcbf3906bb056b
Salvador <Salvador316Alvarez@gmail.com>
2019-12-24 15:53:58 -0400

    Rock Paper Scissors Browser Game

---
+++ test.js
@@ -0,0 +1,29 @@
+//Rock, Paper, Scissors Game
+
+let check = document.getElementById('check');
+let guess = new URLSearchParams(window.location.search);
+let myAnswer = guess.get('guess');
+let rivalAnswer = Math.random();
+
+    if (rivalAnswer < 0.33) {
+        rivalAnswer = "rock";
+    }
+    else if (rivalAnswer > o.66) {
+        rivalAnswer = "scissors";
+    }
+    else {
+        rivalAnswer = "paper";
+    }
+
+//draw scenario
+if (myAnswer===rivalAnswer) 
+    {check.innerHTML = `Its a draw! You both chose ${myAnswer}!`;}
+//winning scenario
+else if (((myAnswer==="rock")&&(rivalAnswer==="scissors")) ||
+         ((myAnswer==="paper")&&(rivalAnswer==="rock"))    ||
+         ((myAnswer==="scissors")&&(rivalAnswer==="paper")))
+    {let capMyAnswer = myAnswer.charAt(0).toUpperCase() + myAnswer.slice(1);
+    check.innerHTML = `${capMyAnswer} beats ${rivalAnswer}! You win!`;}
+//losing scenario
+else {let capRivalAnswer = rivalAnswer.charAt(0).toUpperCase() + rivalAnswer.slice(1);
+    check.innerHTML = `${capRivalAnswer} beats ${myAnswer}! You win!`;}
